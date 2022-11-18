e3f156bf2d437206c3e646123c2a521f35d11828
can-dy-jack <jjack_chen3@163.com>
2022-11-18 21:16:59 +0800

    修复bug

--- component/functions.js
+++ component/functions.js
@@ -6,19 +6,22 @@ function Functions() {
     const toTop = useRef();
 
     useEffect(() => {
-        let max = document.documentElement.scrollTopMax;
-        window.onscroll = e => {
+        let max = document.documentElement.scrollHeight - document.documentElement.clientHeight;
+        window.onscroll = () => {
             let scroll = document.documentElement.scrollTop;
             updateProgress(
                 '' + Math.floor(scroll/max*100) + '%'
             )
+            // console.log(max, scroll/max*100, scroll)
             if(scroll <= 500) {
                 toTop.current.classList.add("hidden");
             } else {
                 toTop.current.classList.remove("hidden");
             }
         }
-        window.onresize = () => max = document.documentElement.scrollTopMax;
+        window.onresize = () => {
+            max = document.documentElement.scrollHeight - document.documentElement.clientHeight
+        };
     }, []);
 
     return (
@@ -27,7 +30,7 @@ function Functions() {
                 () => window.scrollTo({ top: 0, behavior: "smooth"})
             }>
                 <div className={styles.function_part}>
-                    <svg width="24px" height="24px" viewBox="0 0 24 24" stroke-width="1.5" fill="none"  xmlns="http://www.w3.org/2000/svg"><path d="M6 11l6-6 6 6M6 19l6-6 6 6" stroke="var(--font)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+                    <svg width="24px" height="24px" viewBox="0 0 24 24" strokeWidth="1.5" fill="none"  xmlns="http://www.w3.org/2000/svg"><path d="M6 11l6-6 6 6M6 19l6-6 6 6" stroke="var(--font)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path></svg>
                 </div>
             </div>
 
@@ -50,12 +53,12 @@ function Functions() {
                 }
             }>
                 <div className={styles.function_part}>
-                    <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
-                        <path d="M12 18a6 6 0 100-12 6 6 0 000 12zM22 12h1M12 2V1M12 23v-1M20 20l-1-1M20 4l-1 1M4 20l1-1M4 4l1 1M1 12h1" stroke="var(--font)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <svg width="24px" height="24px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+                        <path d="M12 18a6 6 0 100-12 6 6 0 000 12zM22 12h1M12 2V1M12 23v-1M20 20l-1-1M20 4l-1 1M4 20l1-1M4 4l1 1M1 12h1" stroke="var(--font)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
                     </svg>
 
-                    <svg width="24px" height="24px" stroke-width="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
-                        <path d="M3 11.507a9.493 9.493 0 0018 4.219c-8.507 0-12.726-4.22-12.726-12.726A9.494 9.494 0 003 11.507z" stroke="var(--font)" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <svg width="24px" height="24px" strokeWidth="1.5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000">
+                        <path d="M3 11.507a9.493 9.493 0 0018 4.219c-8.507 0-12.726-4.22-12.726-12.726A9.494 9.494 0 003 11.507z" stroke="var(--font)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"></path>
                     </svg>
                 </div>
             </div>
