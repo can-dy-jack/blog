387c3fa36c55bb836cd9c4b5d4d0407bc9867636
can-dy-jack <jjack_chen3@163.com>
2022-12-17 15:32:52 +0800

    更新文章

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,14 +1,16 @@
-import Layout from "../../component/layout"
-import SEO from "../../component/SEO"
+import Layout from "../../component/layout";
+import { useRouter } from "next/router";
+import SEO from "../../component/SEO";
 import {
   getAllBlogIds,
   getBlogsData,
   getSortedBlogsData,
-} from "../../lib/getblog"
-import styles from "../../styles/blogs.module.css"
-import Link from "next/link"
+} from "../../lib/getblog";
+import styles from "../../styles/blogs.module.css";
+import Link from "next/link";
 
 function BlogPages({ data, allBlogs }) {
+  const router = useRouter();
   return (
     <>
       <SEO
@@ -40,18 +42,24 @@ function BlogPages({ data, allBlogs }) {
           <aside className={styles.foot_box}>
             <h3>最近的博文</h3>
             <div className={styles.more_blog}>
-              {allBlogs.map((blog) => (
-                <div
-                  key={blog.slug}
-                  style={{
-                    "--bg": blog.img
-                      ? `url(${blog.img})`
-                      : `url("/img/default4.jpg")`,
-                  }}
-                >
-                  <Link href={blog.slug}>{blog.title}</Link>
-                </div>
-              ))}
+              {allBlogs.map((blog) => {
+                if (blog.slug !== router.query.slug) {
+                  return (
+                    <div
+                      key={blog.slug}
+                      style={{
+                        "--bg": blog.img
+                          ? `url(${blog.img})`
+                          : `url("/blog/img/default2.jpg")`,
+                      }}
+                    >
+                      <Link href={blog.slug}>{blog.title}</Link>
+                    </div>
+                  );
+                } else {
+                  return null;
+                }
+              })}
             </div>
           </aside>
         </section>
